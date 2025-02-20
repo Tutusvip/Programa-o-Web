7d301c2213db9f60f47984ee53506e42de08247b
tutusvip <111453250+Tutusvip@users.noreply.github.com>
2025-02-20 20:44:34 -0300

    Add files via upload

---
+++ ProgramaçãoWeb2.0/aulasjs2002/prog2.js
@@ -0,0 +1,7 @@
+var nm = print('Digite o nome ');
+if(nm==="maria"){
+    document.write("O nome digitado foi " +nm+" maria");
+}else{
+    document.write("O nome digitado foi " +nm);
+    console.log("KKKKKKKKK" +nm);
+}
